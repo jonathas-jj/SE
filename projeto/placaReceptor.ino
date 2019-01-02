e257856aa15d8682ebb01a74a82f83898bfa2256
jonathas-jj <42251280+jonathas-jj@users.noreply.github.com>
2019-01-02 14:58:09 -0200

    Add files via upload

--- projeto/placaReceptor.ino
+++ projeto/placaReceptor.ino
@@ -1,11 +1,22 @@
 // placa que vai receber e comparar os dados vindos ,via rádio, da placa com o receptor 
 // Essa placa também conterá um modulo bluetooth que se comunicará com um smartphone enviando informações sobre os dois 
 //código do emissor
+
+
+//biblioteca display
+#include <stdio.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+//Definição de transmissão
+#define BAUD (F_CPU/16/9600-1)
+
+//biblioteca radiohead
 #include <RH_ASK.h>
 
+//biblioteca sensor de temperatura
 #include <DHT_U.h>
 #include <DHT.h>
-
  
 #define sensorTemp 10
 #define LED 4 //pino 7 = porta E5
@@ -17,30 +28,81 @@ RH_ASK driver(2000, 11, 12); // define o transmissor na porta 12 e o receptor na
 // Definicoes do sensor : pino e tipo. É uma classe.
 DHT dht(sensorTemp, DHTTYPE);
 
+// declarações e definições pro display
+
+#define OLED_MOSI  38
+#define OLED_CLK   40
+#define OLED_DC    34
+#define OLED_CS    32
+#define OLED_RESET 36
+
+Adafruit_SSD1306 dsp(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+// buffer de recebimento de rádio
+
+uint8_t buflen = 11 ; //tamanho da mensagem
+int enviados=0; // dados enviados
+uint8_t buf[11]; //declara um buffer (de tamanhoRH_ASK_MAX_MESSAGE_LEN) para armazenar a mensagem 
+String msg;    
+int tamanhoMsg; 
+
+
 void setup() {
   
   Serial.begin(9600); 
+  // aloca dinamicamente a mensagem
+  msg="";
+  tamanhoMsg=0;
+  enviados=0;
+  cli();
+  UBRR0H = BAUD >> 8;
+  UBRR0L = BAUD;
+  /* Enable transmitter */
+   UCSR0A = 0;
+  UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
+  UCSR0C |= (1<<UCSZ01) | (1 <<UCSZ00);
+  UCSR0B |= (1<<RXCIE0) |
+  (1<<TXCIE0) ;
+  
+  sei();
+  
+
+  
   DDRH=1<<LED;
   // Iniclaiza o sensor DHT (sensor de temperatura e humidade)
   dht.begin();
   //testa o modulo de  transmissão/recepção
-    if (!driver.init()) Serial.println("Falha no inicio da recepção RF");
+    if (!driver.init()); //Serial.println("Falha no inicio da recepção RF");
+
+  dsp.begin(SSD1306_SWITCHCAPVCC, 0x3C);//inicia o display com endereco padrao
+  dsp.clearDisplay();//limpa a tela
+ 
+ 
+  dsp.setTextColor(WHITE);//define o texto para branco (no display ficara azul)
+ 
+  dsp.setTextSize(1);//define o tamanho do texto
+  dsp.println("Sistemas Embarcados");//escreve na tela a mensagem
+ 
+  dsp.setTextSize(3);
+  dsp.println("2018");
+  dsp.display();//mostra as alteracoes no display, sem isso nao ira mostrar nada!!
+  //delay(1000);
+  dsp.clearDisplay();
+  
+  
 }
-float charPraFloat(char *c, int ini, int fim ){
-  char aux[fim-ini];int j=0;
-  for(int i=ini;i<fim;i++){
-       aux[j]=c[i];
-       j++;
-      }
-  return atof(aux);
-  }
+
+
+
+
+  
 void loop() {
 
 
   // Aguarda 2 segundos entre as medicoes
   PORTH ^=1<<LED;
   delay(2000);
- 
+  tamanhoMsg=0;
   // Leitura da umidade
   float h = dht.readHumidity();
   
@@ -49,47 +111,100 @@ void loop() {
   
    if (isnan(h) || isnan(t))//se nao ler um número
   {
-   
-    Serial.println("Falha ao ler dados do sensor DHT !!!");
+    
+   // Serial.println("Falha ao ler dados do sensor DHT !!!");
+    imprimeNaTela(0,0);
     return;
   }
-  float t_emissor,h_emissor;
-  char *aux;
-  uint8_t buf[11]; //declara um buffer (de tamanhoRH_ASK_MAX_MESSAGE_LEN) para armazenar a mensagem 
-  uint8_t buflen = sizeof(buf); //tamanho da mensagem
-  uint8_t i; //contador  de caracteres do buffer
-  aux=(char*)buf;
+  imprimeNaTela(t,h);
   
-    if (driver.recv(buf, &buflen)) { // se recebeu uma mensagem
-      
-      t_emissor= charPraFloat(aux, 6, 10);//corta a mensagem e converte pra float
-      h_emissor=charPraFloat(aux, 0, 4);//corta a mensagem e converte pra float
+  float t_emissor,h_emissor;
+  
+  
+  uint8_t i; //contador  de caracteres do buffer
+  
+
+
+  if (driver.recv(buf, &buflen)) { // se recebeu uma mensagem
+       //msg=(char*)buf;
      
-     /* if( ((t_emissor-t)> 10.0) ||  ((t_emissor-t) < -10.0) ){
+         
+         tamanhoMsg=msg.length();  
        
-        Serial.println("Atenção!");
-        Serial.println("Temperaturas dos ambientes incongruentes!");
-        
+        t_emissor= charPraFloat((char *)buf, 6, 10);//corta a mensagem e converte pra float
+        h_emissor=charPraFloat((char *)buf, 0, 4);//corta a mensagem e converte pra float
+
+        if((t-t_emissor)>5){
+          encheMensagem(t,h,2);
+        }else{
+           encheMensagem(t,h,1);
         }
-       if( ((h_emissor-h)> 10.0) ||  ((h_emissor-h) < -10.0) ){
-       
-        Serial.println("Atenção!");
-        Serial.println("humidades dos ambientes incongruentes!");
-        }*/
+        UDR0=msg[0]+'0';
          
-         
-      Serial.print("Temperatura emissor/receptor:"); Serial.print(t_emissor);Serial.print("°C/");Serial.print(t);Serial.println("°C");
-      Serial.print("Humidade emissor/receptor:"); Serial.print(h_emissor);Serial.print("%/");Serial.print(h);Serial.println("%");
+        
+    
      
       
     }else{
-      Serial.println("Nenhuma Mensagem recebida do trasmissor");
-      Serial.print("Temperatura receptor:"); Serial.print(t);Serial.println("°C");
-      Serial.print("Humidade receptor:"); Serial.print(h);Serial.println("%");
+       encheMensagem(t,h,0);
+       tamanhoMsg=msg.length();  
+       UDR0=msg[0];
+         
+    
+      
       }
       
       
-    
-    
+ 
   
 }
+
+
+//Enche mensagem a ser enviada via bluetooth
+void encheMensagem(float temperatura, float humidade, bool flag){
+   if(flag==2) {
+       msg="ATENÇÃO! DISCREPANCIA ENTRE AS MEDIDAS DE TEMPERATURA \n";
+       msg+="Temperatura emissor/receptor: "+String(charPraFloat((char *) buf, 6, 10 ),DEC)+" / "+String(temperatura,DEC) + "/n";
+       msg=msg + "Humidade emissor/receptor: "+String(charPraFloat((char *) buf, 0, 4 ),DEC)+" / "+String(humidade,DEC) + "/n";
+    }else if(flag==1){
+     msg="Temperatura emissor/receptor: "+String(charPraFloat((char *) buf, 6, 10 ),DEC)+" / "+String(temperatura,DEC) + "/n";
+     msg=msg + "Humidade emissor/receptor: "+String(charPraFloat((char *) buf, 0, 4 ),DEC)+" / "+String(humidade,DEC) + "/n";
+    }else if(flag==0){
+      msg="Nenhuma Mensagem recebida do trasmissor\nTemperatura receptor:"+String(temperatura,DEC)+" Humidade receptor:"+String(humidade,DEC)+"\n";
+      }
+  
+  }
+
+ //decodifica mensagem recebida via rádio. 
+float charPraFloat(char *c, int ini, int fim ){
+  char aux[fim-ini];int j=0;
+  for(int i=ini;i<fim;i++){
+       aux[j]=c[i];
+       j++;
+      }
+  return atof(aux);
+  }
+
+
+ISR (USART0_TX_vect) {
+   enviados++;
+   if(enviados>=tamanhoMsg){enviados=0;return;}
+    UDR0 = msg[enviados];
+            
+    
+}
+void imprimeNaTela(float t , float h){
+  dsp.clearDisplay();
+  
+   dsp.setTextColor(WHITE);
+   dsp.setTextSize(2);
+   String str ="dsa" ;
+   dsp.print("Temp:");
+   dsp.println(t);
+   dsp.setTextSize(1);
+   dsp.print("Humidade: ");
+   dsp.println(h);
+   dsp.setCursor(0, 0);
+   dsp.display();//mostra na tela 
+  
+  }
